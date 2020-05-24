294f334aa85ed3c07f47942363eadbf3b84eb480
shuhari <shuhari@outlook.com>
2020-05-24 14:35:30 +0800

    Implement for block for template engine project.

---
+++ template_engine/step04_for_block/template.py
@@ -0,0 +1,298 @@
+import re
+import typing
+
+
+OUTPUT_VAR = "_output_"
+INDENT = 1
+UNINDENT = -1
+INDENT_SPACES = 2
+INDEX_VAR = "index"
+
+
+class LoopVar:
+    def __init__(self, index: int):
+        self.index = index
+        self.index0 = index
+        self.index1 = index + 1
+
+
+class CodeBuilder:
+    """Manage code generating context."""
+    def __init__(self):
+        self.codes = []
+        self._block_stack = []
+
+    def add_code(self, line: str):
+        self.codes.append(line)
+
+    def add_expr(self, expr: str):
+        code = f"{OUTPUT_VAR}.append(str({expr}))"
+        self.codes.append(code)
+
+    def add_text(self, text: str):
+        code = f"{OUTPUT_VAR}.append({repr(text)})"
+        self.codes.append(code)
+
+    def indent(self):
+        self.codes.append(INDENT)
+
+    def unindent(self):
+        self.codes.append(UNINDENT)
+
+    def code_lines(self):
+        indent = 0
+        for code in self.codes:
+            if isinstance(code, str):
+                prefix = ' ' * indent * 2 * INDENT_SPACES
+                line = prefix + code
+                yield line
+            elif code in (INDENT, UNINDENT):
+                indent += code
+
+    def source(self) -> str:
+        return "\n".join(self.code_lines())
+
+    def check_code(self):
+        if self._block_stack:
+            last_control = self._block_stack.pop(-1)
+            raise SyntaxError(f"{last_control.name} has no end tag")
+
+    def push_control(self, ctrl):
+        self._block_stack.append(ctrl)
+
+    def end_block(self, begin_token_type):
+        block_name = begin_token_type.name
+        if not self._block_stack:
+            raise SyntaxError(f'End of block {block_name} does not found matching start tag')
+        top_block = self._block_stack.pop(-1)
+        if type(top_block) != begin_token_type:
+            raise SyntaxError(f'Expected end of {block_name} block, got {top_block.name}')
+        return top_block
+
+
+class Token:
+    """Token in template code"""
+    def parse(self, content: str):
+        raise NotImplementedError()
+
+    def generate_code(self, builder: CodeBuilder):
+        raise NotImplementedError()
+
+
+class Text(Token):
+    def __init__(self, content: str = None):
+        self._content = content
+
+    def parse(self, content: str):
+        self._content = content
+
+    def generate_code(self, builder: CodeBuilder):
+        builder.add_text(self._content)
+
+    def __repr__(self):
+        return f"Text({self._content})"
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self._content == other._content
+
+
+class Expr(Token):
+    def __init__(self, varname: str = None):
+        self._varname = varname
+        self._filters = []
+
+    def parse(self, content: str):
+        self._varname, self._filters = parse_expr(content)
+
+    def generate_code(self, builder: CodeBuilder):
+        result = self._varname
+        for filter_name in self._filters[::-1]:
+            result = f"{filter_name}({result})"
+        builder.add_expr(result)
+
+    def __repr__(self):
+        if self._filters:
+            return f"Expr({self._varname} | {' | '.join(self._filters)})"
+        return f"Expr({self._varname})"
+
+    def __eq__(self, other):
+        return type(self) == type(other) and \
+               self._varname == other._varname and \
+               self._filters == other._filters
+
+
+class Comment(Token):
+    def __init__(self, content: str = None):
+        self._content = content
+
+    def parse(self, content: str):
+        self._content = content
+
+    def __repr__(self):
+        return f"Comment({self._content})"
+
+    def __eq__(self, other):
+        return type(self) == type(other) and \
+            self._content == other._content
+
+    def generate_code(self, builder: CodeBuilder):
+        pass
+
+
+class For(Token):
+    name = 'for'
+
+    def __init__(self, var_name: str = None, target: str = None):
+        self._varname = var_name
+        self._target = target
+
+    def parse(self, content: str):
+        m = re.match(r'for\s+(\w+)\s+in\s+(\w+)', content)
+        if not m:
+            raise SyntaxError(f"Invalid for block: {content}")
+        self._varname, self._target = m.group(1), m.group(2)
+
+    def __repr__(self):
+        return f"For({self._varname} in {self._target})"
+
+    def __eq__(self, other):
+        return type(self) == type(other) and \
+            self._varname == other._varname and \
+            self._target == other._target
+
+    def generate_code(self, builder: CodeBuilder):
+        builder.add_code(f"for {INDEX_VAR}, {self._varname} in enumerate({self._target}):")
+        builder.indent()
+        builder.push_control(self)
+        builder.add_code(f"loop = LoopVar({INDEX_VAR})")
+
+
+class EndFor(Token):
+    def parse(self, content: str):
+        pass
+
+    def __repr__(self):
+        return 'EndFor'
+
+    def __eq__(self, other):
+        return type(self) == type(other)
+
+    def generate_code(self, builder: CodeBuilder):
+        builder.unindent()
+        builder.end_block(For)
+
+
+def extract_last_filter(text: str) -> (str, str):
+    """
+    Extract last filter from expression like 'var | filter'.
+    return (var, None) when no more filters found.
+    """
+    m = re.search(r'(\|\s*[A-Za-z0-9_]+\s*)$', text)
+    if m:
+        suffix = m.group(1)
+        filter_ = suffix[1:].strip()
+        var_name = text[:-len(suffix)].strip()
+        return var_name, filter_
+    return text, None
+
+
+def parse_expr(text: str) -> (str, typing.List[str]):
+    """
+    Parse expression to variable name and filters.
+    for example, "name | upper | strip" will be converted to 'name', [ 'upper', 'strip']
+    """
+    var_name, filters = text, []
+    while True:
+        var_name, filter_ = extract_last_filter(var_name)
+        if filter_:
+            filters.insert(0, filter_)
+        else:
+            break
+    return var_name, filters
+
+
+def create_control_token(text: str) -> Token:
+    """Create control token() from source code fragment."""
+    text = text.strip()
+    m = re.match(r'^(\w+)', text)
+    if not m:
+        raise SyntaxError(f'Unknown control token: {text}')
+    prefix = m.group(1)
+    token_types = {
+        'for': For,
+        'endfor': EndFor,
+    }
+    if prefix not in token_types:
+        raise SyntaxError(f'Unknown control token: {text}')
+    return token_types[prefix]()
+
+
+def create_token(text: str) -> Token:
+    """Create token from source code fragment."""
+    if text.startswith("{{") and text.endswith("}}"):
+        token, content = Expr(), text[2:-2].strip()
+    elif text.startswith("{%") and text.endswith("%}"):
+        content = text[2:-2].strip()
+        token = create_control_token(content)
+    elif text.startswith("{#") and text.endswith("#}"):
+        token, content = Comment(), text[2:-2].strip()
+    else:
+        token, content = Text(), text
+    token.parse(content)
+    return token
+
+
+def tokenize(text: str) -> typing.List[Token]:
+    """Parse template text to tokens."""
+    segments = re.split(r'({{.*?}}|{#.*?#}|{%.*?%})', text)
+    segments = [x for x in segments if x]
+    return [create_token(x) for x in segments]
+
+
+class Template:
+    """Render template in flask-like syntax."""
+    def __init__(self, text: str, filters: dict = None):
+        self._text = text
+        self._code = None
+        self._global_vars = {}
+        if filters:
+            self._global_vars.update(filters)
+
+    def _generate_code(self):
+        """Generate to compiled code if not done yet."""
+        if not self._code:
+            tokens = tokenize(self._text)
+            builder = CodeBuilder()
+            for token in tokens:
+                token.generate_code(builder)
+            builder.check_code()
+            self._code = compile(builder.source(), '', 'exec')
+
+    def render(self, ctx: dict) -> str:
+        """bind context and generate result text"""
+        self._generate_code()
+        output = []
+        exec_ctx = (ctx or {}).copy()
+        exec_ctx.update({
+            OUTPUT_VAR: output,
+            'LoopVar': LoopVar,
+        })
+        exec(self._code, self._global_vars, exec_ctx)
+        return "".join(output)
+
+
+class TemplateEngine:
+    """Factory class to create Template object."""
+    def __init__(self):
+        self._filters = {}
+        self._register_default_filters()
+
+    def register_filter(self, name: str, filter_):
+        self._filters[name] = filter_
+
+    def _register_default_filters(self):
+        self.register_filter('upper', lambda x: x.upper())
+        self.register_filter('strip', lambda x: x.strip())
+
+    def create(self, text: str) -> Template:
+        return Template(text, filters=self._filters)
---
+++ template_engine/step04_for_block/test_template.py
@@ -0,0 +1,155 @@
+import unittest
+
+from .template import Template, TemplateEngine, tokenize, parse_expr, Text, Expr, Comment, For, EndFor
+
+
+class TokenizeTest(unittest.TestCase):
+    def test_single_variable(self):
+        tokens = tokenize("Hello, {{name}}!")
+        self.assertEqual(tokens, [
+            Text("Hello, "),
+            Expr("name"),
+            Text("!")
+        ])
+
+    def test_two_variables(self):
+        tokens = tokenize("Hello, {{name}} in {{year}}")
+        self.assertEqual(tokens, [
+            Text("Hello, "),
+            Expr("name"),
+            Text(" in "),
+            Expr("year")
+        ])
+
+    def test_comment(self):
+        tokens = tokenize("Prefix {# Comment #} Suffix")
+        self.assertEqual(tokens, [
+            Text("Prefix "),
+            Comment("Comment"),
+            Text(" Suffix"),
+        ])
+
+    def test_parse_repr(self):
+        cases = [
+            ("name", "name", []),
+            ("name | upper", "name", ["upper"]),
+            ("name | upper | strip", "name", ["upper", "strip"]),
+            ("'a string with | inside' | upper | strip", "'a string with | inside'", ["upper", "strip"])
+        ]
+        for expr, varname, filters in cases:
+            parsed_varname, parsed_filters = parse_expr(expr)
+            self.assertEqual(varname, parsed_varname)
+            self.assertEqual(filters, parsed_filters)
+
+    def test_tokenize_for_loop(self):
+        tokens = tokenize("{% for row in rows %}Loop {{ row }}{% endfor %}")
+        self.assertEqual(tokens, [
+            For("row", "rows"),
+            Text("Loop "),
+            Expr("row"),
+            EndFor(),
+        ])
+
+    def test_tokenize_invalid_control(self):
+        with self.assertRaises(SyntaxError):
+            tokenize("{% nokeyword %}")
+
+
+class TemplateTest(unittest.TestCase):
+    def render(self, text: str, ctx: dict, expected: str, filters: dict = None):
+        engine = TemplateEngine()
+        if filters:
+            for filter_name, fn in filters.items():
+                engine.register_filter(filter_name, fn)
+        template = engine.create(text)
+        rendered = template.render(ctx)
+        self.assertEqual(expected, rendered)
+
+    def test_plain_text(self):
+        for text in [
+            'This is a simple message.',
+            '<h1>This is a html message.</h1>',
+            'This is a multi line message\nThis is line 2 of the message',
+        ]:
+            self.render(text, None, text)
+
+    def test_expr_single(self):
+        self.render("Hello, {{name}}!",
+                    {"name": "Alice"},
+                    "Hello, Alice!")
+
+    def test_expr_array(self):
+        self.render("Hello, {{names[0]}}!",
+                    {"names": ["guest"]},
+                    "Hello, guest!")
+
+    def test_expr_array(self):
+        self.render("Hello, {{names['guest']}}!",
+                    {"names": {"guest": 123}},
+                    "Hello, 123!")
+
+    def test_expr_multi(self):
+        self.render("Hello, {{user}} at {{year}}!",
+                    {"user": "Alice", "year": 2020},
+                    "Hello, Alice at 2020!")
+
+    def test_expr_variable_missing(self):
+        with self.assertRaises(NameError):
+            self.render("{{name}}", {}, "")
+
+    def test_expr_with_filter_1(self):
+        self.render("Hello, {{ name | upper }}!",
+                    {"name": "Alice"},
+                    "Hello, ALICE!")
+
+    def test_expr_with_filter_2(self):
+        self.render("Hello, {{ name | upper | strip }}!",
+                    {"name": "  Alice  "},
+                    "Hello, ALICE!")
+
+    def test_expr_with_addition_filter(self):
+        first = lambda x: x[0]
+        self.render("Hello, {{ name | upper | first }}!",
+                    {"name": "Alice"},
+                    "Hello, A!",
+                    filters={"first": first})
+
+    def test_comment(self):
+        self.render("Hello, {# This is a comment. #}World!",
+                    {},
+                    "Hello, World!")
+
+    def test_comment_with_expr(self):
+        self.render("Hello, {# This is a comment. #}{{name}}!",
+                    {"name": "Alice"},
+                    "Hello, Alice!")
+
+    def test_render_for_loop(self):
+        self.render("{% for msg in messages %}Item {{msg}}!{% endfor %}",
+                    {"messages": ["a", "b", "c"]},
+                    "Item a!Item b!Item c!")
+
+    def test_render_for_loop_with_index(self):
+        self.render("{% for msg in messages %}{{loop.index1}}.{{msg}}!{% endfor %}",
+                    {"messages": ["a", "b", "c"]},
+                    "1.a!2.b!3.c!")
+
+    def test_render_for_loop_no_start_tag(self):
+        with self.assertRaises(SyntaxError):
+            self.render("{% endfor %}",
+                        {"messages": ["a", "b", "c"]},
+                        "")
+
+    def test_render_for_loop_no_end_tag(self):
+        with self.assertRaises(SyntaxError):
+            self.render("{% for msg in messages %}{{msg}}",
+                        {"messages": ["a", "b", "c"]},
+                        "")
+
+
+def main():
+    unittest.main(__name__)
+
+
+if __name__ == '__main__':
+    main()
